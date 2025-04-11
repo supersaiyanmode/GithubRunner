b902b63fb7cfbb29f2af767e159182307e9b7df7
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2025-04-11 17:21:03 +0530

    Add debug log line.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ if [[ -z "${GITHUB_TOKEN}" ]]; then
   exit 1
 fi
 
+echo "Using Github token: ${GITHUB_TOKEN:0:2}..${GITHUB_TOKEN:1:-1}"
+
 if ! ./config.sh --url https://github.com/supersaiyanmode/GithubRunner --token $GITHUB_TOKEN; then
   echo "Failed to connect to GitHub, exiting .." 1>&2
   exit 1
